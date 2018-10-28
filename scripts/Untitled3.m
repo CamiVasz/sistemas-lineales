a849763f910565e2f202e4ee6c1ca13b404e5bc4
CamiVasz <31051511+CamiVasz@users.noreply.github.com>
2018-10-28 17:41:08 -0500

    Add files via upload

--- scripts/Untitled3.m
+++ scripts/Untitled3.m
@@ -1,5 +1,5 @@
 
-%===== Archivo de ejemplo para la linealización y obtención de funciones de
+%===== Archivo de ejemplo para la linealizaciï¿½n y obtenciï¿½n de funciones de
 %transferencia utilizando MATLAB=====
 %NOTA: Se debe tener instalado el Toolbox Symbolic Math
 
@@ -8,20 +8,20 @@
 %====Primero se declaran las variables del modelo como simbolicas====
 syms x c ce u
 
-%==== Se definen los parámetros del modelo 
+%==== Se definen los parï¿½metros del modelo 
 
 syms a n b h r0 r1 p k0 M mu la
-% a  = 0.01;
-% n  = 0.01;
-% b  = 0.2;
-% h  = 0.2;
-% r0 = 0.1;
-% r1 = 0.01;
-% p  = 2;
-% k0 = 5;
-% M  = 0.5;
-% mu = 0.1;
-% la = 0.01;
+
+n  = 0.01;
+b  = 0.2;
+h  = 0.2;
+r0 = 0.1;
+r1 = 0.01;
+p  = 2;
+k0 = 5;
+M  = 0.5;
+mu = 0.1;
+la = 0.01;
 
 
 %==== Se escriben las ecuaciones del modelo ====
@@ -34,12 +34,11 @@ FF = [dx dce dc]; % Vector de Ecuaciones
 
 %==== Se define el vector de salidas ====
 
-Y= x; %Ecuación de la salida
+Y= x; %Ecuaciï¿½n de la salida
 
 %==== Se declaran las variables de estado y entradas ====
 x = [x ce c]; % Variables de estado- Variables que se encuentran en las derivadas del modelo
-U = [u];  % Vector de
-entradas
+U = [u];  % Vector de entradas
 
 %==== Se realiza la derivadas parciales por medio del comando
 %jacobian() del vector de ecuaciones con respecto de las variables de estado y las entradas====
@@ -48,19 +47,160 @@ BM = jacobian(FF,U);
 C = jacobian(Y,x);
 D = jacobian(Y,u);
 
-%==== Se define el punto de operación al rededor del cual se realizará
-%lalinealización ====
+%==== Se define el punto de operaciï¿½n al rededor del cual se realizarï¿½
+%lalinealizaciï¿½n ====
 u = 4;
 x = 2.9;
 c = 6.8;
 ce = 23.4;
 
-%==== Se realiza la lineación en el punto de operación ====
+%==== Se realiza la lineaciï¿½n en el punto de operaciï¿½n ====
 A = eval(A);
 BM = eval(BM);
 C = eval(C);
 D = eval(D);
 
-[As, BMs, Cs, Ds] = linmodv5('biomasaLineal', [x ce c], 4)
+a  = 0.01;
+A
+sys = ss(A,BM,C,D)
+G = tf(sys)
+GD = c2d(G,0.5)
 
-plot(tsim, xnl, tsim, xli)
+figure
+step(sys)
+hold on
+step(G)
+hold on 
+step(GD)
+
+figure
+impulse(sys)
+hold on
+impulse(G)
+hold on 
+impulse(GD)
+
+T = 0:0.5:180;
+uu = sin(T) + 4;
+figure
+lsim(sys, uu, T)
+hold on
+lsim(G, uu, T)
+hold on 
+lsim(GD, uu, T)
+
+% Funcion de ponderacion discreta
+syms z
+zz = [z z^2 z^3 z^4];
+[num, den] = tfdata(GD, 'v');
+iz = (zz * num')/(zz * den');
+% transformada inversa z
+ff = iztrans(iz);
+fpd = zeros(10,1);
+% Se hallan los primeros 10 tÃ©rminos
+for i = 1:11
+    n = sym(i-1);
+    n0 = eval(ff);
+    fdp(i) = eval(n0);
+end
+
+fdp = [-0.000001416533741415915,
+    -0.00000825786463191854,
+    -0.00002384029688964825,
+    -0.000048047247438898,
+    -0.00007930902293923858,
+    -0.0001162670126816055,
+    -0.0001577471214501992,
+    -0.0002027364960132934,
+    -0.0002503631382311275,
+    -0.0002998780480342695,
+    -0.0003506395835840654];
+T = 0:0.5:5;
+plot(0:0.25:2.5, fdp, 'o')
+hold on
+%figure
+[x,y] = impulse(GD,T)
+
+
+% reduccion
+%syms s
+[num, den] = tfdata(G, 'v');
+G0 = num(end)/den(end);
+den = (s-0.0474 - 0.0156i)*(s-0.0474 + 0.0156i);
+dr = flip(eval(coeffs(den)));
+%K = G0 / (num(end) / den);
+K = (-9.89*10^(-5)*dr(end))/0.000655;
+aa = pole(G)
+aa = aa(2:end);
+rs = tf(zpk([], aa, K));
+step(rs);
+hold on
+step(G)
+hold off
+
+T = 0:0.5:180;
+uu = sin(T) + 4;
+figure
+lsim(rs, uu, T)
+hold on
+lsim(G, uu, T)
+
+
+GR = tf(reduce(G,2));
+step(GR);
+hold on
+step(G)
+hold off
+
+T = 0:0.5:180;
+uu = sin(T) + 4;
+figure
+lsim(GR, uu, T)
+hold on
+lsim(G, uu, T)
+
+Gz = (-1.971*10^(-6)*z^2 - 7.54*10^(-6)*z - 1.803*10^(-6))/(z^3 - 2.83*z^2 + 2.666*z - 0.8361)
+iz = iztrans(Gz)
+
+fpd4 = zeros(10,1);
+for i = 1:11
+    n = sym(i-1);
+    n0 = eval(iz);
+    fdp4(i) = eval(n0);
+end
+fdp2 = [-3.7947e-19,
+    -1.9710e-06,
+    -1.3118e-05,
+    -3.3672e-05,
+    -6.1967e-05,
+    -9.6566e-05,
+    -1.3623e-04,
+    -1.7990e-04,
+    -2.2666e-04,
+    -2.7574e-04,
+    -3.2648e-04]
+fdp3 = [0.,-1.971*10^-6,-0.0000131179,-0.0000336721,-0.0000619675,-0.0000965661,-0.00013623,-0.000179897,-0.000226658,-0.000275738,-0.000326481];
+T = 0:0.5:5;
+plot(T, fdp4, 'o')
+hold on
+%figure
+impulse(GD,T)
+
+% Lugar de las raices
+s = tf('s');
+GRL = (2.9*s^2 +0.4*s + 0.0008)/(s^3 + 0.3*s^2 + 0.028*s + 0.0006)
+rlocus(GRL)
+
+%Diagrama de bode
+bode(G)
+% modelo reducido
+bode(rs)
+
+%modelo discretizado
+bode(GD)
+
+T = 0:0.5:180;
+uu = sin(0.1*T);
+lsim(GD, uu, T)
+
+margin(GD)
